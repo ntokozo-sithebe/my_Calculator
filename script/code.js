ebd656b7ed4811afa7588d878da9f5ca927989c3
ntokozo-sithebe <ntoky28@gmail.com>
2023-11-12 21:35:17 +0200

    java&others

--- script/code.js
+++ script/code.js
@@ -1,2 +1,29 @@
-const operators = document.querySelector('#signs')
-addEventListener
+function operationsChoice(){
+
+}
+
+function remove(){
+
+}
+
+function number(){
+
+}
+function updateNum(){
+
+}
+function display(){
+
+}
+
+
+
+
+
+
+const numberButtons = document.querySelectorAll(['data-number'])
+const cancelButton = document.querySelector(['data-cancel'])
+const equalButton = document.querySelector(['data-equal'])
+const answer = document.querySelector(['data-equal'])
+const operationButtons = document.querySelectorAll(['data-operation'])
+
